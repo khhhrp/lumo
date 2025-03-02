3c1276b58e478eeb91b4eeed9be76dc1413fc4dd
vdmkhrp <vdmkhrp@gmail.com>
2025-03-03 01:10:44 +0200

    created several new sections

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const burgerButton = document.querySelector(".burger-btn");
+  const headerMenu = document.querySelector(".header__menu");
+
+  function activeToggle(element, toggleClass) {
+    element.classList.toggle(toggleClass);
+  }
+
+  function removeActiveClasses() {
+    burgerButton.classList.remove("burger-btn--active");
+    headerMenu.classList.remove("header__menu--active");
+  }
+
+  burgerButton.addEventListener("click", () => {
+    activeToggle(burgerButton, "burger-btn--active");
+    activeToggle(headerMenu, "header__menu--active");
+  });
+
+  window.addEventListener("resize", () => {
+    removeActiveClasses();
+  });
+});
