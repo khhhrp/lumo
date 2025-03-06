8b6d3bb716468cfe0da52fad56db065caeb75f8d
vdmkhrp <vdmkhrp@gmail.com>
2025-03-06 19:20:55 +0200

    fix menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const burgerButton = document.querySelector(".burger-btn");
   const headerMenu = document.querySelector(".header__menu");
+  const body = document.body;
 
   function activeToggle(element, toggleClass) {
     element.classList.toggle(toggleClass);
@@ -9,11 +10,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
   function removeActiveClasses() {
     burgerButton.classList.remove("burger-btn--active");
     headerMenu.classList.remove("header__menu--active");
+    body.classList.remove("no-scroll");
   }
 
   burgerButton.addEventListener("click", () => {
     activeToggle(burgerButton, "burger-btn--active");
     activeToggle(headerMenu, "header__menu--active");
+    body.classList.toggle("no-scroll");
   });
 
   window.addEventListener("resize", () => {
